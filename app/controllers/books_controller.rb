d7031dde8718542e9510655e992b4a650edc0d6c
EC2 Default User <ec2-user@ip-172-31-18-190.ap-northeast-1.compute.internal>
2022-07-10 09:37:53 +0000

    editページ完成?

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -13,8 +13,15 @@ class BooksController < ApplicationController
     end
   end
 
+  def update
+    book = Book.find(params[:id])
+    book.update(book_params)
+    redirect_to book_path(book.id)
+  end
+
+
   def index
-    @book = Book.all
+    @books = Book.all
   end
 
   def edit
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
   get '/edit' => "books#edit"
   get "/books" => "books#top"
 
+  patch 'books/:id' => 'books#update', as: 'update_book'
+
   resources :books
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
